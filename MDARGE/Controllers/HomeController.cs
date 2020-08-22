1a92727749aa5ddbbf8de4190d7aa15519f2dd66
Tunayylmz <tunaysabriyilmaz@gmail.com>
2020-08-22 15:15:10 +0300

    ty

--- MDARGE/Controllers/HomeController.cs
+++ MDARGE/Controllers/HomeController.cs
@@ -7,15 +7,23 @@ using System.Web.Mvc;
 using static System.Net.Mime.MediaTypeNames;
 using System.Net;
 using System.Net.Mail;
+using System.Data.SQLite;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.ComponentModel;
 
 namespace MDARGE.Controllers
 {
+
     public class HomeController : Controller
     {
+        SQLiteConnection baglan = new SQLiteConnection();
+
+
         // GET: Home
         public ActionResult index()
         {
- 
+
             return View();
         }
         public ActionResult about()
@@ -39,7 +47,7 @@ namespace MDARGE.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult contact(string txtname, string txtemail,string txtphone, string txtsubject, string txtmessage)
+        public ActionResult contact(string txtname, string txtemail, string txtphone, string txtsubject, string txtmessage)
         {
             System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
             SmtpClient sc = new SmtpClient();
@@ -49,8 +57,8 @@ namespace MDARGE.Controllers
             MailMessage msg = new MailMessage("info@mdarge.com", "info@mdarge.com");
             tny.Credentials = new NetworkCredential("info@mdarge.com", "OmUr4379");
             sc.Credentials = new NetworkCredential("info@mdarge.com", "OmUr4379");
-            
-            msg.Body ="Konu: "+ txtsubject + "\n\r" + "Ad-Soyad: " + txtname + "\n\r" + "E-Posta: " + txtemail + "\n\r" + "Telefon: " + txtphone + "\n\r" + "Mesaj: " + txtmessage;
+
+            msg.Body = "Konu: " + txtsubject + "\n\r" + "Ad-Soyad: " + txtname + "\n\r" + "E-Posta: " + txtemail + "\n\r" + "Telefon: " + txtphone + "\n\r" + "Mesaj: " + txtmessage;
             msg.From = new MailAddress("info@mdarge.com", txtemail);
             msg.Subject = "Contact Us: " + txtsubject;
             tny.Send(msg);
@@ -62,7 +70,7 @@ namespace MDARGE.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Faq( string txtname, string txtemail, string txtsubject, string txtmessage)
+        public ActionResult Faq(string txtname, string txtemail, string txtsubject, string txtmessage)
         {
             System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
             SmtpClient sc = new SmtpClient();
@@ -152,9 +160,62 @@ namespace MDARGE.Controllers
         {
             return View();
         }
-
+        public ActionResult smartQaUnique3()
+        {
+            return View();
+        }
         public ActionResult download()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [Obsolete]
+        public ActionResult download(string key)
+        {
+            TempData["mesaj"] = null;
+            try
+            {
+                string ip = Request.UserHostAddress;
+                string browser = Request.Browser.Browser;
+                DateTime now = DateTime.Now;
+                string a = now.ToString("yyyyMMddhhmmss");
+
+                string conString = "Data Source = www.mdarge.com\\MSSQLSERVER2017; Initial Catalog = SoftwareLicenses; Persist Security Info=True;User ID = DCube; Password=tUnAy2020";
+                SqlConnection baglanti = new SqlConnection(conString);
+                baglanti.Open();
+                string sqlStr = "select * from dcubeSetupKeys where SoftwareKey='" + key + "'";
+
+                SqlCommand com = new SqlCommand(sqlStr, baglanti);
+                SqlDataReader reader = com.ExecuteReader();
+                System.Data.DataTable dt = new System.Data.DataTable();
+                dt.Load(reader);
+
+                if (dt.Rows.Count > 0)//key dogru
+                {
+                    if (dt.Rows[0]["Browser"] is System.DBNull && dt.Rows[0]["IPAddress"] is System.DBNull && dt.Rows[0]["DownloadDateTime"] is System.DBNull)
+                    {
+                        WebClient webClient = new WebClient();
+                        webClient.DownloadFile("http://www.devbg.org/img/Logo-BASD.jpg", @"C:\\Users\\user\\Desktop\\Deneme\\tny.jpg");
+                        string save = "update  dcubeSetupKeys set Browser='" + browser + "',IPAddress='" + ip + "', DownloadDateTime='" + a + "'where SoftwareKey='" + key + "'";
+                        com = new SqlCommand(save, baglanti);
+                        com.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        TempData["mesaj"] = "Wrong password! Please try again.";
+                    }
+                }
+                else//key yanlış
+                {
+                    TempData["mesaj"] = "Wrong password! Please try again.";
+                }
+            }
+
+            catch (Exception)
+            {
+                TempData["mesaj"] = "Wrong password! Please try again.";
+            }
             return View();
         }
     }
--- MDARGE/Controllers/query.cs
+++ MDARGE/Controllers/query.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Data.OleDb;
 
 namespace MDARGE.Controllers
 {
